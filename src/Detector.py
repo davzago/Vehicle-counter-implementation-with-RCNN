a1d4a72d2eec25de4766f0f7c24fe207e66aad7a
Davide <zago-7@live.it>
2020-10-14 15:23:49 +0200

    detector is now memory efficient

--- src/Detector.py
+++ src/Detector.py
@@ -6,34 +6,35 @@ import numpy as np
 class Detector():
     def __init__(self, path_to_frame_folder):
         self.rects = []
-        self.images = []
         self.diff_images = []
         frames = os.listdir(path_to_frame_folder)
         frames.sort(key=lambda f: int(re.sub('\D', '', f)))
-        for i in frames:
-            img = cv2.imread(path_to_frame_folder + '/' + i)
-            self.images.append(img)
+        img1 = 0
+        img2 = cv2.imread(path_to_frame_folder + '/' + str(0) + ".jpg")
+        for i in range(0,len(frames)-1):
+            img1 = img2
+            img2 = img2 = cv2.imread(path_to_frame_folder + '/' + str(i+1) + ".jpg")
+            diff = self.img_diff(img1, img2)
+            self.threshold_and_dilate(diff)
 
     # loops over the frames of the video to obtain the difference between consecutive frames    
-    def img_diff(self):
-        for i in range (0,len(self.images)-1):
-            # first we convert the frame to grayscale
-            frame1 = cv2.cvtColor(self.images[i], cv2.COLOR_BGR2GRAY)
-            frame2 = cv2.cvtColor(self.images[i+1], cv2.COLOR_BGR2GRAY) # added skip frame to have better differences
-            # then we compute the difference between the 2 images
-            diff = cv2.absdiff(frame1,frame2)
-            self.diff_images.append(diff)
+    def img_diff(self, img1, img2):
+        # first we convert the frame to grayscale
+        frame1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        frame2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY) # added skip frame to have better differences
+        # then we compute the difference between the 2 images
+        diff = cv2.absdiff(frame1,frame2)
+        return diff
     
     # loops over the difference images and applies thresholding and dilate in order to find contours
     # thresholding is basically set a threshold, the pixels which value is lower than the threshold are set to 0
     # the ones which values is higher are set to 255
     # dilatation is used in order to unify fragmented regions and help the detection
-    def threshold_and_dilate(self):
-        for i in range(0,len(self.diff_images)):
-            _, thresh = cv2.threshold(self.diff_images[i], 55, 255, cv2.THRESH_BINARY)
-            kernel = np.ones((3, 3),np.uint8)
-            dilated = cv2.dilate(thresh, kernel, iterations=1)
-            self.diff_images[i] = dilated
+    def threshold_and_dilate(self, diff):
+        _, thresh = cv2.threshold(diff, 55, 255, cv2.THRESH_BINARY)
+        kernel = np.ones((3, 3),np.uint8)
+        dilated = cv2.dilate(thresh, kernel, iterations=1)
+        self.diff_images.append(dilated)
 
     def save_diff_images(self, path):
         is_dir = os.path.isdir(path) 
@@ -62,8 +63,6 @@ class Detector():
             self.rects.append(rec)
     
     def detect(self, path_to_diff):
-        self.img_diff()
-        self.threshold_and_dilate()
         self.draw_bbox()
         self.save_diff_images(path_to_diff)
 
--- src/ToFrame.py
+++ src/ToFrame.py
@@ -11,4 +11,4 @@ def video_to_frame(path_to_video, path_to_frame_folder):
         print('Read a new frame: ', success)
         count += 1
 
-video_to_frame("data/video2.mp4", "data/frames")
+video_to_frame("data/video1.mp4", "data/frames")
