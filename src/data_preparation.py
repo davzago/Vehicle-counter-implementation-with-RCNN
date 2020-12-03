f38932c640ecad37fd22e85510c2cbcbf30b50cc
Davide <zago-7@live.it>
2020-12-03 17:29:24 +0100

    function for dataset creation works!

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -44,12 +44,22 @@ def get_bbox(file_path):
 	return np.array(rectangles)
 
 def get_bbox_json(file_path):
-	"""data = pd.read_json(file_path, lines=True)#file_path, sep=',', usecols=['ImageID', 'XMin', 'XMax', 'YMin', 'YMax'])
-	print(data)"""
 	with open(file_path) as f: 
 		d = json.load(f)
 	
-	df = json_normalize(d[1]["objects"])
+	df = json_normalize(d)
+	rectangles = []
+	for _, row in df.iterrows():
+		frame_number = row["frameNumber"] - 1 
+		objects_json = row["objects"]
+		obj_df = json_normalize(objects_json)
+		for _, o in obj_df.iterrows():
+			box = [frame_number, int(o["bbox.left"]), int(o["bbox.top"]), int(o["bbox.width"]+o["bbox.left"]), int(o["bbox.height"]+o["bbox.top"])]
+			rectangles.append(box)
+	return rectangles
+			
+			
+		
 	
 	print(df.head(3))
 	"""rectangles = []
@@ -71,12 +81,11 @@ def compute_iou(box1, box2):
 
 	return intersection / (area1 + area2 - intersection)
 
-"""def update_dataset(img_path, dataset_path, bbox_path, max_pos, max_neg, total_pos, total_neg):
-	ground_truth = # take csv and read
+def update_dataset(img_path, ground_truth, dataset_path, max_pos, max_neg, total_pos, total_neg):
 	s_search = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
 	img = cv2.imread(img_path)
-    s_search.setBaseImage(img)
-    s_search.switchToSelectiveSearchFast()
+	s_search.setBaseImage(img)
+	s_search.switchToSelectiveSearchFast()
 	# selective search returns a tuple like this (x, y , w, h)
 	r = s_search.process()
 	rectangles = []
@@ -87,30 +96,41 @@ def compute_iou(box1, box2):
 	del r
 	count_pos = 0
 	count_neg = 0
-	for truth_box in ground_truth:
+	for (_, x, y, x1, y1) in ground_truth:
+		truth_box = [x, y, x1, y1]
 		for sel_box in rectangles:
 			iou = compute_iou(truth_box, sel_box)
 			if count_pos < max_pos and iou > 0.7:
-				w = sel_box[0] - sel_box[2]
-				h = sel_box[1] - sel_box[3]
+				w = sel_box[2] - sel_box[0]
+				h = sel_box[3] - sel_box[1]
 				center = (int(sel_box[0]+w / 2), int(sel_box[1]+h / 2))
 				c = cv2.getRectSubPix(img, (w, h), center)
+				c = cv2.resize(c, (224,224), interpolation=cv2.INTER_CUBIC)
 				cv2.imwrite(dataset_path + '/positive/' + str(total_pos) + '.jpg', c)
 				total_pos+=1
 				count_pos+=1
 			
-			fulloverlap = sel_box[0] >= truth_box[0] and sel_box[1] >= truth_box[1] and sel_box[2] <= truth_box[2]
-							and sel_box[3] <= truth_box[3]
+			fulloverlap = sel_box[0] >= truth_box[0] and sel_box[1] >= truth_box[1] and sel_box[2] <= truth_box[2] and \
+				sel_box[3] <= truth_box[3]
 			
 			if not fulloverlap and iou < 0.1 and count_neg < max_neg:
-				w = sel_box[0] - sel_box[2]
-				h = sel_box[1] - sel_box[3]
+				w = sel_box[2] - sel_box[0]
+				h = sel_box[3] - sel_box[1]
 				center = (int(sel_box[0]+w / 2), int(sel_box[1]+h / 2))
 				c = cv2.getRectSubPix(img, (w, h), center)
+				c = cv2.resize(c, (224,224), interpolation=cv2.INTER_CUBIC)
 				cv2.imwrite(dataset_path + '/negative/' + str(total_neg) + '.jpg', c)
 				total_neg+=1
 				count_neg+=1
 	
-	return total_pos, total_neg"""
+	return total_pos, total_neg
 
-get_bbox_json("LABELSEXAMPLE.json")
+gt = get_bbox_json("LABELSEXAMPLE.json")
+p = 0
+n = 0
+for frame in range(0,99):
+	# 1. estrarre gt per il frame in esame
+	img_gt = list(filter(lambda x: x[0]==frame, gt))
+	# 2. update dataset
+	p, n = update_dataset("data/frames/"+str(frame)+".jpg", img_gt, "dataset", 30, 30, p, n)
+	
